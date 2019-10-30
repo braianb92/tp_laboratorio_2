ad9f9cee0ca8e6da401e7ae264dd266d89097f0a
alumno <alumno@utnfra.com.ar>
2019-10-29 22:13:45 -0300

    tp3

---
+++ TP3/Baldino.Braian.2D.TP3/Clases Abstractas/Persona.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excepciones;
+
+namespace Clases_Abstractas
+{
+    public abstract class Persona
+    {
+        public enum ENacionalidad
+        {
+            Argentino,
+            Extranjero
+        }
+
+        private string nombre;
+        private string apellido;
+        private ENacionalidad nacionalidad;
+        private int dni;
+
+        public string Nombre
+        {
+            get { return this.nombre; }
+            set { this.nombre = value; }
+        }
+
+        public string Apellido
+        {
+            get { return this.apellido; }
+            set { this.apellido = value; }
+        }
+
+        public ENacionalidad Nacionalidad
+        {
+            get { return this.nacionalidad; }
+            set { this.nacionalidad = value; }
+        }
+
+        public int DNI
+        {
+            get { return this.dni; }
+            set
+            {
+                this.dni = ValidarDni(this.Nacionalidad, value);
+            }
+        }
+
+        public string StringToDni
+        {
+            set
+            {
+                this.dni = ValidarDni(this.Nacionalidad, value);
+            }
+        }
+
+        private int ValidarDni(ENacionalidad nacionalidad, int dato)
+        {
+            switch (nacionalidad)
+            {
+                case ENacionalidad.Argentino:
+                    if (dato < 1 || dato > 89999999)
+                    {
+                        dato = 0;
+                        throw new NacionalidadInvalidaException("DNI no valido para Argentina");
+                    }
+                    break;
+                case ENacionalidad.Extranjero:
+                    if (dato < 90000000 || dato > 99999999)
+                    {
+                        dato = 0;
+                        throw new NacionalidadInvalidaException("DNI no valido para Extranjero");
+                    }
+                    break;
+            }
+            return dato;
+        }
+
+        private int ValidarDni(ENacionalidad nacionalidad, string dato)
+        {
+            if (!string.IsNullOrEmpty(dato) && int.TryParse(dato, out int datoInt))
+            {
+                return ValidarDni(nacionalidad, datoInt);
+            }
+            else
+            {
+                throw new DniInvalidoException();
+            }
+        }
+    }
+}
