61077d355fc26ca1fa6c916bca80d2b1e730d4c3
Baldino Braian <baldino.bab@gmail.com>
2019-11-02 18:30:26 -0300

    tp_3 02

--- TP3/Baldino.Braian.2D.TP3/Clases Instanciables/Jornada.cs
+++ TP3/Baldino.Braian.2D.TP3/Clases Instanciables/Jornada.cs
@@ -6,7 +6,93 @@ using System.Threading.Tasks;
 
 namespace Clases_Instanciables
 {
-    class Jornada
+    public class Jornada
     {
+        #region Atributes
+        private List<Alumno> alumnos;
+        private Universidad.EClases clase;
+        private Profesor instructor;
+        #endregion
+
+        #region Properties
+        public List<Alumno> Alumnos
+        {
+            get { return alumnos; }
+            set { alumnos = value; }
+        }
+
+        public Universidad.EClases Clase
+        {
+            get { return clase; }
+            set { clase = value; }
+        }
+
+        public Profesor Instructor
+        {
+            get { return instructor; }
+            set { instructor = value; }
+        }
+        #endregion
+
+        #region Constructors
+        private Jornada()
+        {
+            this.alumnos = new List<Alumno>();
+        }
+
+        public Jornada(Universidad.EClases clase, Profesor instructor) : this()
+        {
+            Clase = clase;
+            Instructor = instructor;
+        }
+        #endregion
+
+        #region Operator Overloads
+        public static bool operator == (Jornada j, Alumno a)
+        {
+            if (a == j.Clase)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator != (Jornada j, Alumno a)
+        {
+            return !(j==a);
+        }
+
+        public static Jornada operator + (Jornada j, Alumno a)
+        {
+            if(j != a)
+                j.Alumnos.Add(a);
+
+            return j;
+        }
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Clase.ToString());
+            sb.AppendLine(Instructor.ToString());
+            foreach (Alumno alumno in Alumnos)
+            {
+                sb.AppendLine(alumno.ToString());
+            }
+            
+            return sb.ToString();
+        }
+
+        public bool Guardar(Jornada jornada)
+        {
+            return true;
+        }
+
+        public string Leer()
+        {
+            return "";
+        }
+        #endregion
     }
 }
--- TP3/Baldino.Braian.2D.TP3/Clases Instanciables/Universidad.cs
+++ TP3/Baldino.Braian.2D.TP3/Clases Instanciables/Universidad.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Excepciones;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,5 +18,138 @@ namespace Clases_Instanciables
             SPD
         }
         #endregion
+
+        #region Atributes
+        private List<Alumno> alumnos;
+        private List<Profesor> profesores;
+        private List<Jornada> jornadas;
+        #endregion
+
+        #region Properties
+        public List<Alumno> Alumnos
+        {
+            get { return this.alumnos; }
+            set { alumnos = value; }
+        }
+
+        public List<Profesor> Profesores
+        {
+            get { return this.profesores; }
+            set { profesores = value; }
+        }
+
+        public List<Jornada> Jornadas
+        {
+            get { return this.jornadas; }
+            set { jornadas = value; }
+        }
+
+        public Jornada this [int i]
+        {
+            get
+            {
+                return this.jornadas[i];
+            }
+            set
+            {
+                this.jornadas[i] = value;
+            }
+        }
+        #endregion
+
+        #region Methods
+        public bool Guardar(Universidad uni)
+        {
+            return true;
+        }
+
+        public Universidad Leer()
+        {
+            return this;
+        }
+
+        private string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (Alumno a in Alumnos)
+            {
+                sb.AppendLine(a.ToString());   
+            }
+
+            foreach (Profesor p in Profesores)
+            {
+                sb.AppendLine(p.ToString());
+            }
+
+            foreach (Jornada j in Jornadas)
+            {
+                sb.AppendLine(j.ToString());
+            }
+
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return MostrarDatos();
+        }
+        #endregion
+
+        #region Operator Overloads
+        public static bool operator == (Universidad g,Alumno a)
+        {
+            foreach (Alumno item in g.Alumnos)
+            {
+                if (item == a)
+                    return true;
+            }
+            return false;
+        }
+
+        public static bool operator !=(Universidad g, Alumno a)
+        {
+            return !(g == a);
+        }
+
+        public static bool operator == (Universidad g, Profesor p)
+        {
+            foreach (Jornada jornada in g.Jornadas)
+            {
+                if (p == jornada.Instructor)
+                    return true;
+            }
+            return false;
+        }
+
+        public static bool operator !=(Universidad g, Profesor p)
+        {          
+            return !(g==p);
+        }
+
+        public static Profesor operator == (Universidad u, EClases clase)
+        {
+            foreach (Profesor profesor in u.Profesores)
+            {
+                if (profesor == clase)
+                    return profesor;
+            }
+
+            throw new SinProfesorException();
+
+        }
+
+        public static Profesor operator !=(Universidad u, EClases clase)
+        {
+            foreach (Profesor profesor in u.Profesores)
+            {
+                if (profesor == clase)
+                    return profesor;
+            }
+
+            throw new SinProfesorException();
+        }
+        #endregion
+
     }
+
 }
