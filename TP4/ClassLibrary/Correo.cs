b9a2c39362eb76daf11fbe05ecae42e3d9b0eb02
alumno <alumno@utnfra.com.ar>
2019-11-12 21:58:15 -0300

    tp4 first commit

---
+++ TP4/ClassLibrary/Correo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Correo
+    {
+        private List<Thread> mockPaquetes;
+        private List<Paquete> paquetes;
+    }
+}
